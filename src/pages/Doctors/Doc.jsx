be27ae029863b00004cd4d674629ef4f7cf89922
hydi-official <esseldacosta00@gmail.com>
2024-07-24 14:23:35 +0000

    doc image updated

--- src/pages/Doctors/Doc.jsx
+++ src/pages/Doctors/Doc.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import home1 from '../../assets/images/DocA.webp';
+import home1 from '../../assets/images/docA.webp';
 import home2 from '../../assets/images/DocB.webp';
 import home3 from '../../assets/images/DocC.avif';
 import home4 from '../../assets/images/DOcD.jpg';
